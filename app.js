b7f91030cd278c11d0d9087dcebee673e7979d96
Patrick Hao <patrick@arblabs.com>
2019-03-15 16:03:43 -0400

    Backend now set up

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mysql = require("mysql");
+var cors = require('cors');
+var bodyParser = require('body-parser')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -20,18 +22,26 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//By Pass CORS
+app.use(cors());
 //Database connection
+var db_config = {
+    host     : '23.229.194.226',
+    user     : 'remote_access',
+    password : 'Lp_147896325',
+    database: 'lp_base',
+};
+
 app.use(function(req, res, next){
-    res.locals.connection = mysql.createConnection({
-        host     : '23.229.194.226',
-        user     : 'remote_access',
-        password : 'Lp_147896325',
-        database: 'lp_base',
-    });
+    res.locals.connection = mysql.createConnection(db_config);
     res.locals.connection.connect();
     next();
 });
 
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
 app.use('/', indexRouter);
 app.use('/api/users', usersRouter);
 
@@ -40,15 +50,17 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
+
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
+  res.locals.connection.connect();
   // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
+
 
 module.exports = app;
