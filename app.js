dc2fec33f012ad03a002360adac019e76bf175ce
Patrick Hao <patrick@arblabs.com>
2019-03-15 11:16:40 -0400

    Database connection setup

---
+++ app.js
@@ -0,0 +1,54 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var mysql = require("mysql");
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Database connection
+app.use(function(req, res, next){
+    res.locals.connection = mysql.createConnection({
+        host     : '23.229.194.226',
+        user     : 'remote_access',
+        password : 'Lp_147896325',
+        database: 'lp_base',
+    });
+    res.locals.connection.connect();
+    next();
+});
+
+app.use('/', indexRouter);
+app.use('/api/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
