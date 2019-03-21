4e5c589d6f7fe904a81a40f61650f0cca8a416da
Patrick Hao <patrick@arblabs.com>
2019-03-21 16:46:00 -0400

    added status feedback and price constants update

--- routes/constants.js
+++ routes/constants.js
@@ -15,4 +15,24 @@ router.post('/price/', function(req, res, next) {
     });
 });
 
+router.post('/price/update/', function(req,res,next){
+    let body = req.body;
+    let success = true;
+    for(let i=0; i<body.length; i++){
+        const data = body[i];
+        res.locals.pool.query("UPDATE price_constants SET service_fee = "+data.service_fee+", government_fee = "+data.government_fee+", misc_fee = "+data.misc_fee+" WHERE id ="+data.id+";", function (error, results, fields) {
+            if(error){
+                res.send(JSON.stringify({"status": 500, "error": error, "response": null}));
+                //If there is error, we send the error in the error section with 500 status
+                success = false;
+            }
+            else{
+                console.log("success - ", data);
+            }
+        });
+    }
+    if(success){
+        res.send(JSON.stringify({"status": 200, "error": null, "response": null}));
+    }
+});
 module.exports = router;
