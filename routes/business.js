6d0215a41f33cfe5552254ef883e92ed182a6064
Patrick Hao <patrick@arblabs.com>
2019-04-06 15:34:14 -0400

    business detail init

--- routes/business.js
+++ routes/business.js
@@ -12,4 +12,17 @@ router.post('/all/', function(req, res, next) {
         }
     });
 });
+
+router.post('/one/', function(req, res, next) {
+    let body = req.body;
+    res.locals.pool.query("SELECT * FROM business WHERE id = "+body.id+"" , function (error, results, fields) {
+        if(error){
+            res.send(JSON.stringify({"status": 500, "error": error, "response": null}));
+            //If there is error, we send the error in the error section with 500 status
+        } else {
+            res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+            //If there is no error, all is good and response is 200OK.
+        }
+    });
+});
 module.exports = router;
