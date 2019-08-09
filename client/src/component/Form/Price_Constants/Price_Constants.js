6d48d77f590ae6130d8816d0f175176acbf2c34a
Serenade092 <serenade092@berkeley.edu>
2019-08-09 13:12:25 -0700

    issue1005

--- client/src/component/Form/Price_Constants/Price_Constants.js
+++ client/src/component/Form/Price_Constants/Price_Constants.js
@@ -3,9 +3,18 @@ import connect from "react-redux/es/connect/connect";
 import * as actionTypes from '../../../store/action';
 import { MDBDataTable } from "mdbreact";
 import "../Form.css";
+import "./Tab.css";
+import DropDown from "../DropDown/DropDown";
+import {Link, withRouter} from "react-router-dom";
+import PriceInput from "./Price_Constants_Add";
+
 class Price_Constants extends Component{
     constructor(props){
         super(props);
+        this.state={
+            add:false,
+            tab: "签证申请"
+        }
     }
 
     componentWillMount() {
@@ -13,8 +22,31 @@ class Price_Constants extends Component{
         this.props.getPriceConstants();
     }
 
+    handleDelete(id){
+        this.props.deleteEntry(id);
+    }
 
-    handleChange(index, e) {
+    handleAdd(){
+        this.setState({
+            add:true,
+            new_service:{
+                name:"",
+                type:"",
+                service_fee:"",
+                government_fee:"",
+                misc_fee:"",
+                copywrite_percentage:""
+            }
+        })
+    }
+
+    changeTab = (e) =>{
+        this.setState({
+            tab:e.target.value
+        })
+    }
+
+    handleChange = (index, e) => {
         console.log(e);
         console.log("index:",index);
         const { name, value } = e.target;
@@ -22,10 +54,12 @@ class Price_Constants extends Component{
         temp[index][name] = value;
         this.props.updatePriceConstants(temp);
     }
+
     handleSubmit(){
         this.props.submitPriceConstants(this.props.constants.fee);
     }
     render(){
+    
         const columns = [
             {
                 label: '具体业务',
@@ -35,7 +69,7 @@ class Price_Constants extends Component{
             },
             {
                 label: '业务类别',
-                field: 'name',
+                field: 'type',
                 sort: 'asc',
 
             },
@@ -54,19 +88,40 @@ class Price_Constants extends Component{
                 field: 'misc_fee',
                 sort: 'asc',
             },
+            {
+                label: '文案比例',
+                field: 'copywrite_percentage',
+                sort: 'asc',
+            },
+            {
+                label: '',
+                field: 'delete_button',
+                sort: 'asc',
+            },
         ];
         let rows = null;
         if(this.props.constants.fee!=null){
             const fee = this.props.constants.fee;
-            rows = Object.keys(fee).map((index)=>{
+            rows = Object.keys(fee).map((index)=>{       
                 return({
                     name: fee[index].name,
                     type:fee[index].type,
                     service_fee: <input className={"mdb-editable"} type={"number"} step={".01"} name={"service_fee"} onChange={this.handleChange.bind(this,index)} value={fee[index].service_fee!=null?fee[index].service_fee:""}/>,
                     government_fee: <input className={"mdb-editable"} type={"number"} step={".01"} name={"government_fee"} onChange={this.handleChange.bind(this,index)} value={fee[index].government_fee!=null?fee[index].government_fee:""}/>,
-                    misc_fee: <input className={"mdb-editable"} type={"number"} step={".01"} name={"misc_fee"} onChange={this.handleChange.bind(this,index)} value={fee[index].misc_fee!=null?fee[index].misc_fee:""}/>
+                    misc_fee: <input className={"mdb-editable"} type={"number"} step={".01"} name={"misc_fee"} onChange={this.handleChange.bind(this,index)} value={fee[index].misc_fee!=null?fee[index].misc_fee:""}/>,
+                    copywrite_percentage: 
+                    <DropDown
+                    options={["10%","20%","30%","40%"]}
+                    name={"copywrite_percentage"}
+                    value={fee[index].copywrite_percentage!=null?fee[index].copywrite_percentage:" "}
+                    handleChange={this.handleChange.bind(this,index)}
+                    />,
+                    delete_button:<button className={"btn btn-danger"} onClick={this.handleDelete.bind(this, fee[index].id)}>删除</button>
                 })
             });
+            if(this.state.tabs!=""){
+                rows = rows.filter(r => {return r.type===this.state.tab});
+            }
         }
         const data = {
             columns:columns,
@@ -77,6 +132,12 @@ class Price_Constants extends Component{
                 <div className={"section-wrapper"}>
                     <div className={"section-header"}>
                         <h3>基础价格常量</h3>
+                        <button className={"tab"} onClick={this.changeTab} value="签证申请">签证申请</button>
+                        <button className={"tab"} onClick={this.changeTab} value="学校申请">学校申请</button>
+                        <button className={"tab"} onClick={this.changeTab} value="换发/公证">换发公证</button>
+                        <button className={"tab"} onClick={this.changeTab} value="移民申请">移民申请</button>
+                        <button className={"tab"} onClick={this.changeTab} value="交通告票">交通告票</button>
+                        <button className={"tab"} onClick={this.changeTab} value="其他">其他</button>
                     </div>
                     <hr className={"style1"}/>
                     <div className={"section-body"}>
@@ -89,7 +150,10 @@ class Price_Constants extends Component{
                         />
                         <div className={"footer"}>
                             <div className={"form-confirmation button-group"}>
-                                <button className={"btn btn-primary"} onClick={this.handleSubmit.bind(this)}>确认</button>
+                            <Link to={"/constants/price/add"}>
+                                <button className={"btn btn-primary"}>添加客户</button>
+                            </Link>                            
+                            <button className={"btn btn-primary"} onClick={this.handleSubmit.bind(this)}>确认</button>
                             </div>
                         </div>
                     </div>
@@ -108,6 +172,7 @@ const mapStateToProps = state => {
 const mapDispatchToProps = dispatch =>{
     return{
         getPriceConstants: () => dispatch({type:actionTypes.SAGA_GET_PRICE_CONSTANTS}),
+        //Delete 
         updatePriceConstants: (constants) => dispatch({type:actionTypes.UPDATE_PRICE_CONSTANTS, constants:constants}),
         submitPriceConstants: (constants) => dispatch({type:actionTypes.SAGA_UPDATE_PRICE_CONSTANTS, constants:constants}),
         popUp: (status, message, action) => dispatch({type:actionTypes.POP_UP, message:message, status:status, action:action}),
---
+++ client/src/component/Form/Price_Constants/Price_Constants_Add.js
@@ -0,0 +1,142 @@
+import React, { Component } from 'react';
+import DropDown from '../DropDown/DropDown';
+import '../Form.css';
+import './PriceDetail.css';
+import Input from "../Input/Input";
+import connect from "react-redux/es/connect/connect";
+import * as actionTypes from "../../../store/action";
+import PhoneInput from "../Input/PhoneInput";
+import CustomFormatInput from "../Input/CustomFormatInput";
+class Price_Constants_Add extends Component{
+    constructor(props){
+        super(props);
+        this.state={
+            detail:{
+                type:"",
+                name:"",
+                service_fee:"",
+                government_fee:"",
+                misc_fee:"",
+                copywrite_percentage:"",
+            },
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+
+    handleChange(e){
+        const { name, value } = e.target;
+        this.setState((prevState) => ({
+            ...prevState,
+            detail:{
+                ...prevState.detail,
+                [name]:value,
+            }
+        }));
+    }
+
+    handleSpecialChange(name,value){
+        this.setState((prevState) => ({
+            ...prevState,
+            detail:{
+                ...prevState.detail,
+                [name]:value,
+            }
+        }));
+    }
+
+    handleSubmit(e){
+        this.props.addPriceConstant(this.state.detail);
+    }
+
+    render(){
+
+        return(
+            <div className={"form-wrapper content-wrapper customer-detail"}>
+                <div className={"section-wrapper"}>
+                    <div className={"section-header"}>
+                        <h3>基础信息</h3>
+                    </div>
+                    <div className={"section-body"}>
+                        <table className={"business-detail-table"}>
+                            <thead/>
+                            <tbody>
+                                <tr>
+                            <td>
+                                <DropDown label={"服务内容"}
+                                    name={"type"}
+                                    value={this.state.detail.type}
+                                    handleChange={this.handleChange}
+                                    options={["签证申请","换发公正","学校申请","移民申请","交通告票 "]}
+                                    />
+                            </td>
+                            <td>
+                                <Input label={"具体业务"}
+                                    name={"name"}
+                                    value={this.state.detail.name}
+                                    type={"text"}
+                                    handleChange={this.handleChange}
+                                    />
+                            </td>
+                            <td>
+                                <DropDown label={"文案比例"}
+                                    name={"copywrite_percentage"}
+                                    value={this.state.detail.copywrite_percentage}
+                                    options={["10%","20%","30%","40%"]}
+                                    handleChange={this.handleChange}
+                                    />
+                            </td>
+                            </tr>
+                            <tr>
+                            <td>
+                                <Input label={"服务费用"}
+                                    name={"service_fee"}
+                                    value={this.state.detail.service_fee}
+                                    type={"text"}
+                                    handleChange={this.handleChange}
+                                    />
+                            </td>
+                            <td>
+                                <Input label={"政府费用"}
+                                    name={"government_fee"}
+                                    value={this.state.detail.government_fee}
+                                    type={"text"}
+                                    handleChange={this.handleChange}
+                                    />
+                            </td>
+                            <td>
+                                <Input label={"邮寄费用"}
+                                    name={"misc_fee"}
+                                    value={this.state.detail.misc_fee}
+                                    type={"text"}
+                                    handleChange={this.handleChange}
+                                    />
+                            </td>
+                            </tr>
+                            </tbody>   
+                    </table> 
+                    </div>
+                </div>
+                <div className={"footer"}>
+                    <div className={"form-confirmation button-group"}>
+                        <button className={"btn btn-primary"} onClick={this.handleSubmit.bind(this)}>添加客户</button>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+const mapStateToProps = state => {
+    return{
+        user:state.user,
+        china_geo:state.china_geo,
+    };
+};
+
+const mapDispatchToProps = dispatch =>{
+    return{
+        addPriceConstant:()=>dispatch(),
+        }
+};
+export default connect(mapStateToProps, mapDispatchToProps)(Price_Constants_Add);
